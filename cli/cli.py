c393b6a70075344d895fc50b947eab60e4d2518d
snaxilian <maximiliansch@gmail.com>
2015-12-18 10:46:20 +0100

    init upload

---
+++ cli/cli.py
@@ -0,0 +1,34 @@
+#import mysql.connector
+import datetime
+import sys
+
+'''
+Ablauf:
+- Aufruf Programm mit Argumenten user, pass, thread-id, read, reply, post-new
+- Auflistung der Optionen: list, read xyz, answer xyz
+
+'''
+
+# get total number of arguments passed
+total = len(sys.argv)
+
+#get arguments list
+cmdargs = str(sys.argv)
+
+# durchgehen aller Argumenten
+
+
+
+#user = input("Please enter your username: ")
+#pass = input("Please enter your password: ")
+
+# Verbinde zur DB
+# cnx_db = mysql.connector.connect(user=$user, password=$pass, host='localhost', database='infosystem')
+
+#print ("")
+
+
+
+#cnx_db.close()
+
+
