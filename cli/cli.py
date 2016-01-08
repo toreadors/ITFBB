4193ec0903962877a34e24f1a2a15483e35dd4bd
snaxilian <maximiliansch@gmail.com>
2016-01-08 11:22:21 +0100

    weitere Inputs & Abklärung DB-Connect
    kwt

--- cli/cli.py
+++ cli/cli.py
@@ -1,4 +1,4 @@
-ubgggggg#import mysql.connector
+import mysql.connector
 import datetime
 import sys
 import argparse
@@ -11,22 +11,28 @@ Ablauf:
 '''
 
 # Parse all the arguments!
-parser = parse.ArgumentParser(description='CLI for itfbb'
+parser = argparse.ArgumentParser(description='CLI for itfbb'
 
-parser.add_argument('-h','--host', help='Hostname',required=True)
-parser.add_argument('-u','--user', help='Username',required=True)
-parser.add_argument('-p','--password', help='Password'required=True)
+parser.add_argument('-h','--host', help='hostname',required=True)
+parser.add_argument('-u','--user', help='username')
+parser.add_argument('-p','--password', help='password')
+
+parser.add_argument('-l','--list', help='list threads')
+parser.add_argument('-r','--read', help='read thread, required ID')
+parser.add_argument('-a','--answer', help='answer to thread, required ID')
+parser.add_argument('-p','--post', help='post new thread')
+
+
 # parser.add_argument('-','--', help='')
 
-
+User Variable in pyhton eingabe ist nur für das Frontend Forum
+im mysql connect sind statische Entrys, oben als VAR festlegen
 
 # Verbinde zur DB
-# cnx_db = mysql.connector.connect(user=$user, password=$pass, host='localhost', database='infosystem')
-
-#print ("")
+connect_db = mysql.connector.connect(user='args.user', password='args.password', host='args.host', database='infosystem')
 
 
 
-#cnx_db.close()
+connect_db.close()
 
 
